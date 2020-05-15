a94bb42daaed0052d11c45b0e22fe096dc2f38f1
Matias Rozanec <rozanecm@gmail.com>
2020-05-15 12:38:58 -0300

    update ejemplo resolver
    muestro caso para obtener solamente la ip de un dominio, que es lo que se necesita en este tp.

--- src/ejemplo_resolver.py
+++ src/ejemplo_resolver.py
@@ -2,5 +2,12 @@ import dns.resolver
 
 # Resolve www.yahoo.com
 result = dns.resolver.query('www.yahoo.com')
+result = dns.resolver.query('yahoo.com')
+
+# Para obtener todo el RR, necesito esto:
 for answer in result.response.answer:
+    print(answer)
+
+# Para obtener solamente la ip, necesito esto:
+for answer in result:
     print(answer)
