03840a0bc90a40607d5fade857f83f87b2fae091
Matias Rozanec <rozanecm@gmail.com>
2020-05-18 09:20:01 -0300

    agregar ejemplo con TTL

--- src/ejemplo_resolver.py
+++ src/ejemplo_resolver.py
@@ -1,13 +1,23 @@
 import dns.resolver
 
 # Resolve www.yahoo.com
-result = dns.resolver.query('www.yahoo.com')
-result = dns.resolver.query('yahoo.com')
+# result = dns.resolver.query('www.yahoo.com')
+# result = dns.resolver.query('yahoo.com')
+urls = ['yahoo.com', 'google.com', 'stackoverflow.com', 'wolframalpha.com', 'lanacion.com', 'infobae.com', 'casarosada.org']
 
-# Para obtener todo el RR, necesito esto:
-for answer in result.response.answer:
-    print(answer)
+for url in urls:
+    print("current url:", url)
+    result = dns.resolver.query(url)
+    print("obteniendo RRs enteros")
+    # Para obtener todo el RR, necesito esto:
+    for answer in result.response.answer:
+        print(answer)
 
-# Para obtener solamente la ip, necesito esto:
-for answer in result:
-    print(answer)
+    print("obteniendo solo IPs")
+    # Para obtener solamente la ip, necesito esto:
+    for answer in result:
+        print(answer)
+
+    print("obteniendo solo TTLs")
+    # Para obtener solamente la ip, necesito esto:
+    print(result.ttl)
